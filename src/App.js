d95331d21827f7029a79d3130cd332e9d5ec57e2
Nipapan Aom <nipapan@maqe.com>
2019-11-26 14:46:59 +0700

    Implementing React.lazy instead of manual code splitting.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
 
-class App extends React.Component {
+class App extends Component {
    constructor() {
       super();
       this.state = {
          route: 'page1',
-         component: ''
       }
    }
 
    onRouteChange = (route) => {
-      if (route === 'page1') {
-         this.setState({ route: route });
-      } else if (route === 'page2') {
-         import('./components/Page2').then((Page2 => {
-            this.setState({route: route, component: Page2.default})
-         }));
-      } else if (route === 'page3') {
-         import('./components/Page3').then((Page3 => {
-            this.setState({route: route, component: Page3.default})
-         }));
-      }
+      this.setState({ route: route });
    }
 
    render() {
       if (this.state.route === 'page1') {
          return <Page1 onRouteChange={this.onRouteChange} />
-      } else {
-         return <this.state.component onRouteChange={this.onRouteChange} />
+      } else if (this.state.route === 'page2') {
+         const PageComponent = React.lazy(() => import('./components/Page2'));
+         return (
+            <Suspense fallback={<div>Loading...</div>}>
+               <PageComponent onRouteChange={this.onRouteChange} />
+            </Suspense>
+         );
+      } else if (this.state.route === 'page3') {
+         const PageComponent = React.lazy(() => import('./components/Page3'));
+         return (
+            <Suspense fallback={<div>Loading...</div>}>
+               <PageComponent onRouteChange={this.onRouteChange} />
+            </Suspense>
+         );
       }
    }
 }
