33834c196dc29ee8689f50bddce582de785b6916
Nipapan Aom <nipapan@maqe.com>
2019-11-26 12:58:43 +0700

    Updates

---
+++ precache-manifest.cae8b0b9182e3c3ccde41799f9c59613.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdb64361ebb62c41962d5b842d44908",
+    "url": "/code-splitting/index.html"
+  },
+  {
+    "revision": "f4b0ef7d829445e6c1b4",
+    "url": "/code-splitting/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "305b030b18b2f1a81558",
+    "url": "/code-splitting/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f4b0ef7d829445e6c1b4",
+    "url": "/code-splitting/static/js/2.8292056f.chunk.js"
+  },
+  {
+    "revision": "73b0836fc9dfeeb50a38",
+    "url": "/code-splitting/static/js/3.d7d3f1c9.chunk.js"
+  },
+  {
+    "revision": "45e1f9b7961f57040241",
+    "url": "/code-splitting/static/js/4.c3d3cc09.chunk.js"
+  },
+  {
+    "revision": "305b030b18b2f1a81558",
+    "url": "/code-splitting/static/js/main.835764da.chunk.js"
+  },
+  {
+    "revision": "fe72c661fc04ed38b185",
+    "url": "/code-splitting/static/js/runtime-main.7f50de65.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/code-splitting/static/media/logo.25bf045c.svg"
+  }
+]);
